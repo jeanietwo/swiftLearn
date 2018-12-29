0f8b4e37adfefcb2a27e8232bc3cddfcf43b2f62
Jean Tu <tujean27@gmail.com>
2018-12-28 21:24:04 -0800

    added the Exercise files from Lynda to track the progress on the course

---
+++ Exercise Files/AdventureSwift_Starter/Ch3_BasicVariables.playground/Pages/Logging and commenting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+/*:
+ # Logging and commenting
+ ---
+ 
+ ## Topic Essentials
+ Commenting your code is the first step to keeping it clean and organized, and the `print` statement is your best friend when it comes to testing that your code is outputing what you expect.
+
+ ### Objectives
+ + Explore single and multiline comment formats
+ + Use the `print` statement to log out a simple string
+ */
+// I'm a single line comment
+
+/* I'm a multiline comment
+ 
+ //nested comment
+ 
+*/
+
+var notHelloWorld = "Hello Swift 4"
+print(notHelloWorld)
+/*:
+ [Previous Topic](@previous)
+ 
+ [Next Topic](@next)
+ */
